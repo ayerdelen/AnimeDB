503a44e29f77bd38288af414dfb38d02b8e2af6a
Aycan <aycanyerdelen@gmail.com>
2021-05-06 15:11:04 +0300

    card has been updated and searchbar has been activated

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ function setQuery(e) {
   }
 }
 
-// function getResults(value) {
-
-// }
-
 const numb = 20;
 //Ekrana yazdıracağım id ye göre ilk 20 apiyi dönüyorum
 const getNumbLoop = async () => {
@@ -29,18 +25,36 @@ const getAnime = async (id) => {
   const url = `https://kitsu.io/api/edge/anime/${id}`;
   const response = await fetch(url);
   const animes = await response.json();
-  createCard(animes);
+  if (animes.data) {
+    createCard(animes.data);
+  }
+};
+
+const getResults = async (value) => {
+  cardSection.innerHTML = "";
+  const url = `https://kitsu.io/api/edge/anime?filter[text]=${value}`;
+  const response = await fetch(url);
+  const search = await response.json();
+  if (search.data) {
+    search.data.forEach((item) => {
+      createCard(item);
+    });
+  }
+  inputValue.value = "";
 };
 
 function createCard(animes) {
-  const animeData = animes.data;
-  console.log(animeData);
-  const { canonicalTitle, posterImage, averageRating } = animeData.attributes;
+  const {
+    canonicalTitle,
+    posterImage,
+    averageRating,
+    type,
+  } = animes.attributes;
   let html = `
   <div class="col-lg-4 col-sm-6 mb-3">
   <div class="card">
 
-    <img src=${posterImage.original} class="card-img-top" alt="..." />
+    <img src=${posterImage.original} class="card-img-top img" alt=${animes.type} />
     <div class="card-body">
       <div class="card-info">
         <h5 class="card-title">${canonicalTitle}</h5>
